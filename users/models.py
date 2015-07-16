4df62fb0be8730039364c517ec23f3be49826b90
Omar Pardo <omarpardog@gmail.com>
2015-07-16 12:37:33 -0500

    Add __str__ and meta to models. Link them by "many to many".

--- users/models.py
+++ users/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
-class Investor(models.Model):
-    name = models.CharField(max_length=200)
-    money = models.IntegerField(default=0)
-
 
 class Bank(models.Model):
     name = models.CharField(max_length=200)
     lower_limit = models.IntegerField(default=0)
     upper_limit = models.IntegerField(default=0)
     interest_rate = models.DecimalField(default=0, max_digits=5, decimal_places=3)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Investor(models.Model):
+    name = models.CharField(max_length=200)
+    money = models.IntegerField(default=0)
+    banks = models.ManyToManyField(Bank)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
