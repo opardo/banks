d714952cc0ee10d1541db863c10ce5bce4b5ca46
Omar Pardo <omarpardog@gmail.com>
2015-07-16 15:23:23 -0500

    Add generate_c function

--- users/models.py
+++ users/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+from cvxopt import matrix
+
 
 class Bank(models.Model):
     name = models.CharField(max_length=200)
     lower_limit = models.IntegerField(default=0)
     upper_limit = models.IntegerField(default=0)
-    interest_rate = models.DecimalField(default=0, max_digits=5, decimal_places=3)
+    interest_rate = models.DecimalField(default=0, max_digits=5, decimal_places=2)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
 
     class Meta:
@@ -19,8 +21,20 @@ class Investor(models.Model):
     money = models.IntegerField(default=0)
     banks = models.ManyToManyField(Bank)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
 
     class Meta:
         ordering = ('name',)
+
+    def generate_c(self):
+
+        c = []
+
+        for bank in self.banks.all():
+            c.append(float(1+bank.interest_rate))
+
+        c = matrix (c)
+
+        return c
+
